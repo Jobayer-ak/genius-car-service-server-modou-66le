dec8dec6efd0acb1bbc3f7b6f6124aed8bef1d71
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-09 21:57:08 +0600

    order api is done

--- index.js
+++ index.js
@@ -53,6 +53,15 @@ async function run() {
     });
 
     // order collection API
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
     app.post("/order", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
