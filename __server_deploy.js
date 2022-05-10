5420dac25bfc3f21b57b41f3a03b4b48bf1d6230
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-10 12:32:44 +0600

    added steps

---
+++ __server_deploy.js
@@ -0,0 +1,12 @@
+/***
+ * ---------------------------
+ * One time for my computer
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * ------------------------------
+ *  For each project one time
+ * ---------------------------
+ * 1. heroku create
+ */
