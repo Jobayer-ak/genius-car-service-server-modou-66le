837c113d362426689d2594928176743c843b2b56
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-10 21:27:54 +0600

    done module 69

--- __server_deploy.js
+++ __server_deploy.js
@@ -21,6 +21,9 @@
  * 2. make sure you: git add . git commit. git push
  * 3. git push heroku main
  * -----------------------------------------------------------------
- * Connect server with client
+ * Connect server with client and deploy client
  * -------------------------------------------------------------
+ * 1. Replace localhost by heroku link
+ * 2. npm run build
+ * 3. firebase deploy
  */
